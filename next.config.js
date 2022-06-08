002ab672b8369d6151fea52b6852c2475c03a28a
smirnovsky <happy280190@gmail.com>
2022-06-08 15:45:10 +0300

    set configs and create components

--- next.config.js
+++ next.config.js
@@ -4,3 +4,31 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+   webpack(config, options) {
+     config.module.rules.push({
+       loader: '@svgr/webpack',
+       issuer: /\.[jt]sx?$/,
+       options: {
+         prettier: false,
+         svgo: true,
+         svgoConfig: {
+           plugins: [{
+             name: 'preset-default',
+             params: {
+               override: {
+                 removeViewbox: false
+               }
+             }
+           }],
+         },
+         titleProp: true,
+       },
+       test: /\.svg$/,
+     
+     });
+
+     return config;
+   },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Htag } from "../components/Htag/Htag";
-  
+import { Button } from "../components/Button/Button";
+import { P, Tag } from "../components/index";
+
+
 export default function Home(): JSX.Element {
   return (
     <>
-       <Htag tag='h1'>text1</Htag>
+      <Htag tag='h1'>text1</Htag>
+      <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <Button appearance='ghost'>Кнопка</Button>
+      <P size='s'>Small</P>
+      <P>Medium</P>
+      <P size='l'>Large</P>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag color='primary'>Primary</Tag>
     </>
   )
 }
