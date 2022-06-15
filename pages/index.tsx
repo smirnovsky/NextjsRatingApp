8ed8ff43c77723a413e13207e2dd0691d5fa9fef
smirnovsky <happy280190@gmail.com>
2022-06-15 17:10:33 +0300

    Add React Hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Htag } from "../components/Htag/Htag";
 import { Button } from "../components/Button/Button";
-import { P, Tag } from "../components/index";
+import { P, Rating, Tag } from "../components/index";
 
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
   return (
     <>
-      <Htag tag='h1'>text1</Htag>
-      <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <Htag tag='h1'></Htag>
+      <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
       <Button appearance='ghost'>Кнопка</Button>
       <P size='s'>Small</P>
       <P>Medium</P>
@@ -17,6 +18,7 @@ export default function Home(): JSX.Element {
       <Tag size='m' color='red'>Red</Tag>
       <Tag size='s' color='green'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
+      <Rating rating={rating} isEditable setRating={setRating}/>
     </>
   )
 }
