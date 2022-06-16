d3f6f598843f15221b5ad761bbc16fb03c6c0f8a
smirnovsky <happy280190@gmail.com>
2022-06-16 18:20:23 +0300

    Added CSS(grid)

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { LayoutProps } from "./Layout.props";
-import styles from './P.module.css';
+import styles from './Layout.module.css';
 import cn from 'classnames'
 import React, { Component } from "react";
 import { Header } from "./Header/Header";
@@ -8,24 +8,22 @@ import { Footer } from "./Footer/Footer";
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header />
-            <div>
-                <Sidebar />
-                <div>
-                    {children}
-                </div>
-            </div>
-            <Footer />
-        </>
-    );  
-} ; 
+        <div className={styles.wrapper}>
+            <Header className={styles.header} />
+            <Sidebar className={styles.sidebar} />
+            <main className={styles.body}>
+                {children}
+            </main>
+            <Footer className={styles.footer} />
+        </div>
+    );
+};
 
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
     return function withLayoutComponent(props: T): JSX.Element {
         return (
             <Layout>
-                <Component {...props}/>
+                <Component {...props} />
             </Layout>
         );
     };
